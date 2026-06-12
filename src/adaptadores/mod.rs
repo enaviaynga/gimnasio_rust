f798285709cf17e5c35fc54e94c3869f46176425
Navia Ynga, Emanuel <enaviaynga@gmail.com>
2026-06-12 14:19:06 -0500

    Refactorizacion base del proyecto

--- src/adaptadores/mod.rs
+++
@@ -1,2 +0,0 @@
-mod entrada;
-mod salida;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
-mod adaptadores;
-mod dominio;
-mod puertos;
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+#![allow(unused_variables, unused_mut, dead_code, unused_imports)]
+
+pub(crate) mod hexagonal;
+pub(crate) mod infraestructura;
+pub mod utiles;
 mod vista;
 
-fn main() {
+use anyhow::Result;
+
+use freya::prelude::*;
+
+use crate::vista::app;
+
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("App iniciada");
+
+    launch(
+        LaunchConfig::new().with_window(
+            WindowConfig::new(move || app(use_state(|| false)))
+                .with_size(900., 600.)
+                .with_title("Gimnasio")
+                .with_min_size(400., 300.),
+        ),
+    );
+
+    Ok(())
 }
